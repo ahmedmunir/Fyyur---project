7c0401997378fa7195fadb89834dd58ab3968054
ahmedmunir <mryagamie@outlook.com>
2020-08-24 16:51:13 +0200

    Add Edit Form GET & POST request for venue and artist and add CSRF TOken to the html files

--- app.py
+++ app.py
@@ -246,55 +246,55 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-  form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
   # TODO: populate form with fields from artist with ID <artist_id>
+  artist = Artist.query.get(artist_id)
+  form = ArtistForm(obj=artist)
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
   # TODO: take values from the form submitted, and update existing
   # artist record with ID <artist_id> using the new attributes
+  edit_artist = Artist.query.get(artist_id)
+  form = ArtistForm(obj=edit_artist)
+  if form.validate_on_submit():
+    edit_artist.name = form.name.data
+    edit_artist.city = form.city.data
+    edit_artist.state = form.state.data
+    edit_artist.phone = form.phone.data
+    edit_artist.genres = form.genres.data
+    edit_artist.facebook_link = form.facebook_link.data
+    db.session.commit()
+    return redirect(url_for('show_artist', artist_id=edit_artist.id))
+  return render_template('forms/edit_artist.html', form=form, artist=edit_artist)
 
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
   # TODO: populate form with values from venue with ID <venue_id>
+  venue = Venue.query.get(venue_id)
+  form = VenueForm(obj=venue)
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
-  return redirect(url_for('show_venue', venue_id=venue_id))
+  edit_venue = Venue.query.get(venue_id)
+  form = VenueForm(obj=edit_venue)
+  if form.validate_on_submit():
+    edit_venue.name = form.name.data
+    edit_venue.city = form.city.data
+    edit_venue.state = form.state.data
+    edit_venue.address = form.address.data
+    edit_venue.phone = form.phone.data
+    edit_venue.genres = form.genres.data
+    edit_venue.facebook_link = form.facebook_link.data
+    db.session.commit()
+    return redirect(url_for('show_venue', venue_id=edit_venue.id))
+  return render_template('forms/edit_venue.html', form=form, venue=edit_venue)
+  
 
 #  Create Artist
 #  ----------------------------------------------------------------
@@ -321,7 +321,6 @@ def create_artist_submission():
     fb_link = request.form['facebook_link']
 
     new_Artist = Artist(name=name, city=city, state=state, phone=phone, genres=genres, facebook_link=fb_link)
-    print(new_Artist)
     db.session.add(new_Artist)
     db.session.commit()
     flash('Artist ' + new_Artist.name + ' was successfully listed!')
